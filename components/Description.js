af460f432f0148e95250cf6f386d351fd21830b2
rosette777 <wkwk2805u@zcube.co.kr>
2023-10-30 21:01:33 +0900

    css 파일 적용

--- components/Description.js
+++ components/Description.js
@@ -1,7 +1,13 @@
+"use client";
 import React from "react";
+import { main, strong } from "./description.module.css";
 
 const Description = () => {
-  return <div>Description</div>;
+  return (
+    <div className={main}>
+      원하는 자산으로 바로 <span className={strong}>교환(스왑)</span>하세요
+    </div>
+  );
 };
 
 export default Description;
