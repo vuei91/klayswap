1a891cebe6ec0e8a630421876f85391d92a95fd2
rosette777 <wkwk2805u@zcube.co.kr>
2023-10-30 21:17:31 +0900

    design 사용

--- app/page.js
+++ app/page.js
@@ -1,11 +1,12 @@
 import Description from "@/components/Description";
 import Ticket from "@/components/Ticket";
 import React from "react";
+import { container, bg } from "./page.module.css";
 
 const Home = () => {
   return (
-    <div>
-      <div className="container">
+    <div className={bg}>
+      <div className={container}>
         <Description />
         <Ticket />
       </div>
--- components/Ticket.js
+++ components/Ticket.js
@@ -1,10 +1,11 @@
 import React from "react";
 import SwapBtn from "./SwapBtn";
 import SwapContent from "./SwapContent";
+import { ticket } from "./ticket.module.css";
 
 const Ticket = () => {
   return (
-    <div className="ticket">
+    <div className={ticket}>
       <SwapContent />
       <SwapBtn />
     </div>
