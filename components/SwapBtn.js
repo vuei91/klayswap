6726003c599e53a047fa618f6f223de60a1e4ce7
rosette777 <wkwk2805u@zcube.co.kr>
2023-10-31 22:02:34 +0900

    exchangeKlayPos

--- components/SwapBtn.js
+++ components/SwapBtn.js
@@ -1,9 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { swapBtn, btnName } from "./swapBtn.module.css";
+import { createWalletClient, custom, getContract } from "viem";
+import { klaytn } from "viem/chains";
+import FactroyABI from "@/abi/FactoryABI.json";
 
 const SwapBtn = () => {
+  useEffect(() => {
+    const walletClient = createWalletClient({
+      chain: klaytn,
+      transport: custom(window.ethereum),
+      account: window.ethereum.selectedAddress,
+    });
+    const Factory = "0xc6a2ad8cc6e4a7e08fc37cc5954be07d499e7654";
+    const _contract = getContract({
+      address: Factory,
+      abi: FactroyABI,
+      walletClient,
+    });
+    setContract(_contract);
+  }, []);
+  const [contract, setContract] = useState();
+  const swap = () => {
+    const OETH = "0x34d21b1e550d73cee41151c77f3c73359527a396";
+    contract.write.exchangeKlayPos({
+      value: BigInt(10e18),
+      args: [OETH, 1, []],
+    });
+  };
   return (
-    <div className={swapBtn}>
+    <div className={swapBtn} onClick={swap}>
       <div className={btnName}>
         <span>Swap</span>
       </div>
